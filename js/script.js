0e06bb6f928aef9f9e0fe7fc73bcf3bd386e5e58
jawadsites <jawadsite4@gmail.com>
2025-01-25 09:20:34 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -126,4 +126,14 @@ window.addEventListener('scroll',() =>{
 
   }
 });
-  
+  <!-- Hotjar Tracking Code for https://jawadsites.github.io/ProTorque-Garage/ -->
+<script>
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:5280417,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+</script>
